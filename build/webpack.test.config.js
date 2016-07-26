667248d174da6d097acb465d4a978d8b64f0e9d0
Zenobius Jiricek <airtonix@gmail.com>
2016-07-26 22:15:25 +0930

    Fix: provide dev config

--- build/webpack.test.config.js
+++ build/webpack.test.config.js
@@ -6,46 +6,11 @@ import cssLoaders from './css-loaders';
 
 export default new Config()
   .extend({
-    './build/webpack.base.config.js': config => {
+    './build/webpack.dev.config.js': config => {
         delete config.entry;
         return config;
       }
   })
   .merge({
     filename: __filename,
-    debug: true,
-    devtool: '#source-map',
-    output: {
-        pathinfo: true
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.vue$/,
-          loader: 'vue'
-        },
-        {
-          test: /\.html$/,
-          loader: 'vue-html'
-        },
-        {
-          test: /\.(png|jpg|gif|svg|woff2?|eot|ttf)(\?.*)?$/,
-          loader: 'url',
-          query: {
-            limit: 10000,
-            name: '[name].[ext]?[hash:7]'
-          }
-        }
-      ]
-    },
-    vue: {
-      loaders: Object.assign(cssLoaders(), {
-        js: 'isparta'
-      })
-    },
-    resolve: {
-      extensions: ['', '.js', '.vue'],
-    }
   });
-  vue: {
-  }
